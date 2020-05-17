4b6ce55dfaf3c5c229fc7ed5123ef9c3d27a97d2
Paul-weqe <paul1tw1@gmail.com>
2020-05-17 14:40:50 +0300

    Enable the sign up controller to sign up a user

--- lib/users/controller.rb
+++ lib/users/controller.rb
@@ -2,13 +2,32 @@ require 'sinatra/base'
 
 
 class MainApp < Sinatra::Base
-  # create user 
-  path = "/user-signup"
-  enable :sessions
+  root_path = File.expand_path('.', Dir.pwd)
+  sign_up_path = "/sign-up"
+  login_path = "/login"
   
-  get path do 
-    "Hello World"
+  enable :sessions
+  set :views, root_path + "/views"
+  
+  # create user
+  get sign_up_path do
+    erb :sign_up
   end
   
+  post sign_up_path do 
+
+    user = User.new
+    user.national_id = params[:national_id]
+    user.first_name = params[:first_name]
+    user.last_name = params[:last_name]
+    user.password = params[:password]
+    user.save
+    puts params
+  end
+   
   # login user
+  get login_path do
+    
+  end
 end
+
